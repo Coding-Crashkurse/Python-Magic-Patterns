55396ee3a6aa02c8de1ab921cd531901506f9b2c
Markus <datamastery87@gmail.com>
2026-01-10 17:18:47 +0100

    Simplify soul swap demo

--- soul_swap.py
+++ soul_swap.py
@@ -8,15 +8,5 @@ def god_mode(x):
 
 damage.__code__ = god_mode.__code__
 
-print(damage(3))      # 2997
-print(damage.__name__)  # "damage" bleibt
-
-
-def banner():
-    return "hello-prod"
-
-
-consts = tuple("hello-staging" if c == "hello-prod" else c for c in banner.__code__.co_consts)
-banner.__code__ = banner.__code__.replace(co_consts=consts)
-
-print(banner())  # "hello-staging"
+print(damage(3))
+print(damage.__name__)
